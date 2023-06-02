ca14cff5ec52f688f26541172c2861dcea113624
Zaki abdullahi <qable2121@gmail.com>
2023-06-02 13:42:11 +0300

    added a 2 image

--- ch-2.js
+++ ch-2.js
@@ -1,34 +1,7 @@
-let std_info =
-
-{
-
-    name: "Zaki",
-    age: 23,
-    school: "Bosaso",
-
-}
-
-
-delete std_info.school;
-console.log(std_info)
-
-
-
-
-
-
 const updateInventory = (inventory, parchasedItems) => {
-
-
-
-
-
     parchasedItems.forEach((item, i) => {
 
-
         if (inventory[item]) {
-
-
             inventory[item] -= 1
             // console.log(inventory[item])
         }
@@ -38,15 +11,11 @@ const updateInventory = (inventory, parchasedItems) => {
 
             delete inventory.bananas;
 
-
-
         }
         if (inventory.apples == 0) {
 
 
             delete inventory.apples;
-
-
 
         }
         if (inventory.oranges == 0) {
@@ -54,19 +23,10 @@ const updateInventory = (inventory, parchasedItems) => {
 
             delete inventory.oranges;
 
-
-
         }
     })
 
-
-
     return inventory;
-
 }
-
 console.log(updateInventory({ "apples": 10, "bananas": 5, "oranges": 8 }, ["apples", "apples", "apples", "bananas", "oranges", "oranges", "oranges", "oranges", "oranges", "oranges", "oranges", "oranges"]));
-
-
-
 // "oranges", "oranges", "oranges", "oranges", "oranges"
